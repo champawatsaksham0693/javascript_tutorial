ec323997f8bcc715450344835b14a128d187790f
Saksham Jain <86518069+champawatsaksham0693@users.noreply.github.com>
2023-10-10 12:27:45 +0000

    operators

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -10,8 +10,8 @@ let state = undefined; //undefined
 // console.log(typeof isEligible);
 
 let inNumber = Number(state)
-console.log(typeof state);
-console.log(inNumber);
+// console.log(typeof state);
+// console.log(inNumber);
 
 /*
     1. When we convert a number into a number then it convert easily 
@@ -22,12 +22,49 @@ console.log(inNumber);
 
 let isEligible = 1
 let isElg = Boolean(isEligible)
-console.log(typeof isEligible);
-console.log(typeof isElg);
-console.log(isElg);
+// console.log(typeof isEligible);
+// console.log(typeof isElg);
+// console.log(isElg);
 
 /*
     1 => true; 34 => true; "saksham" => true
     0 => false; " " => false
  */
+//****************Operators and Operations*********************
 
+let str1 = "Hello ";
+let str2 = "Saksham"
+let str3 = str1 + str2;
+//console.log(str3);
+
+// console.log(2+2); //Addition
+// console.log(2-2); //Subtraction
+// console.log(2*2); //Multiplication
+// console.log(2**3); //Exponent (Power)
+// console.log(2/2); //Division
+// console.log(2%4); //Modulous
+
+//console.log((3+4)*5); //output is 35 (This is the correct way of writing the code because in this we use the paranthesis so it gives us proper output and it also doesn't produce any confusion)
+// console.log("1"+2); //output is 12
+// console.log(1+"2");//output is 12
+// console.log("1"+2+2);//output is 122
+// console.log(1+2+"2");//output is 32
+
+// console.log(+true); //output is 1
+// console.log(+""); //output is 0
+
+let num1, num2, num3;
+num1 = num2 = num3 = (2+2);
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+let counter = 100
+counter++;
+// console.log(counter);
+++counter;
+// console.log(counter);
+counter--;
+// console.log(counter);
+--counter;
+// console.log(counter);
