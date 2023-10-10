c1e0cbea92e945acf180f801c610f57c886fe153
Saksham Jain <86518069+champawatsaksham0693@users.noreply.github.com>
2023-10-10 11:55:09 +0000

    conversion of datatypes

---
+++ basics/conversionOperation.js
@@ -0,0 +1,33 @@
+let score = 33 //number
+let marks = "33"//string
+let temprature = null; //object
+let state = undefined; //undefined
+
+// console.log(typeof score);
+// console.log(typeof marks);
+// console.log(typeof temprature);
+// console.log(typeof state);
+// console.log(typeof isEligible);
+
+let inNumber = Number(state)
+console.log(typeof state);
+console.log(inNumber);
+
+/*
+    1. When we convert a number into a number then it convert easily 
+    2. When we convert this type of number into number means:- "334" => 334 and type convert from string to number 
+    3. When we convert this type of number into number means:- "33abcd" => NaN (Not a Number) it show NaN as output but type is convert from string to number 
+    4. true => 1 ; false => 0;
+*/
+
+let isEligible = 1
+let isElg = Boolean(isEligible)
+console.log(typeof isEligible);
+console.log(typeof isElg);
+console.log(isElg);
+
+/*
+    1 => true; 34 => true; "saksham" => true
+    0 => false; " " => false
+ */
+
